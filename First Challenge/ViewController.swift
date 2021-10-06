8f2dd18a0523e37db14bb9882d46aecd15c089a7
kaelsihombing <sntmcl1@gmail.com>
2021-10-06 16:11:06 +0700

    Initial Commit

---
+++ First Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First Challenge
+//
+//  Created by Santo Michael Sihombing on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
